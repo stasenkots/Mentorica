08b6552c32060a65f1e14e26639baca692976f1e
stasenkots <tstasenko2@gmail.com>
2021-09-18 20:19:35 +0300

    Remove google auth

--- app/src/main/java/com/mentorica/ui/get_started/GetStartedViewModel.kt
+++ app/src/main/java/com/mentorica/ui/get_started/GetStartedViewModel.kt
@@ -1,9 +1,21 @@
 package com.mentorica.ui.get_started
 
 import androidx.lifecycle.ViewModel
+import com.mentorica.nav.NavTarget
+import com.mentorica.nav.Navigator
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class GetStartedViewModel @Inject constructor(): ViewModel() {
+class GetStartedViewModel @Inject constructor(
+    private val navigator: Navigator
+): ViewModel(), Navigator by navigator {
+
+    fun login() {
+        navigator.navigateTo(NavTarget.LoginScreen(NavTarget.LoginScreen.Login))
+    }
+
+    fun register(){
+        navigator.navigateTo(NavTarget.LoginScreen(NavTarget.LoginScreen.Register))
+    }
 }
