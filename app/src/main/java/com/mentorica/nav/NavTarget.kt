e3ae2f79ee6dd6ca2b4cd8bf03cd8e7c1c7ee397
stasenkots <tstasenko2@gmail.com>
2021-09-18 20:22:19 +0300

    Add NavTarget LoginScreen

--- app/src/main/java/com/mentorica/nav/NavTarget.kt
+++ app/src/main/java/com/mentorica/nav/NavTarget.kt
@@ -2,6 +2,13 @@ package com.mentorica.nav
 
 sealed class NavTarget(val label: String) {
 
-    object SplashScreen: NavTarget("splash screen")
+    object SplashScreen: NavTarget("splash")
+    object GetStartedScreen: NavTarget("get started")
+    class LoginScreen(type: Type = None): NavTarget("login/${type.value}") {
+        sealed class Type(val value: String)
+        object Login: Type("login")
+        object Register: Type("register")
+        object None: Type("{login_type}")
+    }
 
 }
