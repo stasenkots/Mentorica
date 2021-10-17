8f309ec9645737091e51c228f5be18600d0ef71d
stasenkots <tstasenko2@gmail.com>
2021-10-17 13:27:59 +0300

    Add Deps CoilImage

--- app/src/main/java/com/mentorica/ui/components/text/ErrorText.kt
+++ app/src/main/java/com/mentorica/ui/components/text/ErrorText.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.skydoves.landscapist.coil.CoilImage
 
 @Composable
 fun ErrorText(text: String) {
