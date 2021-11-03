25e7792e8319374dc5a1668b2295a411aeea4b65
kigya <kirillborichevskiy@gmail.com>
2021-11-03 17:22:03 +0300

    component/MentorItem - Add icons functionality

--- app/src/main/java/com/mentorica/screens/home/MentorItem.kt
+++ app/src/main/java/com/mentorica/screens/home/MentorItem.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,23 +23,36 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.mentorica.R
+import com.mentorica.models.currentUser
+import com.mentorica.models.setCurrentUser
 import com.mentorica.ui.theme.BlueOpacityDark
 import com.mentorica.ui.theme.DarkBlueText
+import com.mentorica.utils.getPaymentFormat
 
 @ExperimentalMaterialApi
 @Composable
-fun MentorItem(user: User, onClick: () -> Unit, addToFavorites: () -> Unit) {
+fun MentorItem(
+    user: User,
+    onClick: () -> Unit,
+    addToFavorites: (String) -> Unit,
+    removeToFavorites: (String) -> Unit,
+) {
     val fullName = "${user.name} ${user.surname}"
-    val status = "${user.position} in ${user.company}"
-    val payment = if(user.payment!! - user.payment.toInt() == 0.0) "${user.payment.toInt()}\$p/h"
-    else "${user.payment}\$p/h"
+    val status = stringResource(R.string.user_status, user.position, user.company)
+    val payment = if(user.payment != null)
+        stringResource(R.string.user_payment, getPaymentFormat(user.payment)) else null
+    var isFavorite by remember { mutableStateOf(currentUser.favorites.contains(user.id)) }
     Card(onClick = onClick) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(120.dp)
                 .background(color = BlueOpacity)
-                .border(width = 0.1.dp, color = BlueOpacity, shape = RoundedCornerShape(15.dp)),
+                .border(
+                    width = 0.1.dp,
+                    color = BlueOpacity,
+                    shape = RoundedCornerShape(15.dp),
+                ),
         ) {
             Spacer(modifier = Modifier.width(15.dp))
             CoilImage(
@@ -76,27 +90,39 @@ fun MentorItem(user: User, onClick: () -> Unit, addToFavorites: () -> Unit) {
                     color = DarkBlueText,
                     fontSize = 12.sp,
                 )
-                Text(
-                    modifier = Modifier.padding(vertical = 8.dp),
-                    text = payment,
-                    fontWeight = FontWeight.SemiBold,
-                    color = DarkBlueText,
-                    fontSize = 16.sp,
-                )
+                if(payment != null) {
+                    Text(
+                        modifier = Modifier.padding(vertical = 8.dp),
+                        text = payment,
+                        fontWeight = FontWeight.SemiBold,
+                        color = DarkBlueText,
+                        fontSize = 16.sp,
+                    )
+                }
             }
             Spacer(modifier = Modifier.width(32.dp))
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 16.dp)
-                    .align(Alignment.CenterVertically)
+                    .align(Alignment.CenterVertically),
             ) {
-                IconButton(modifier = Modifier.size(64.dp),
-                    onClick = { }) {
-                    Icon(modifier = Modifier.size(64.dp),
-                        painter = painterResource(id = R.drawable.ic_baseline_favorite_border_24),
-                        contentDescription = "contentDescription",
-                        tint = BlueOpacityDark)
+                val onClickFavorite = {
+                    if(isFavorite) removeToFavorites(user.id)
+                    else addToFavorites(user.id)
+                }
+                IconButton(
+                    modifier = Modifier.size(64.dp),
+                    onClick = onClickFavorite,
+                ) {
+                    val iconFavoriteId = if(isFavorite) R.drawable.ic_favorite_filled
+                    else R.drawable.ic_favorite_empty
+                    Icon(
+                        modifier = Modifier.size(64.dp),
+                        painter = painterResource(iconFavoriteId),
+                        contentDescription = null,
+                        tint = BlueOpacityDark,
+                    )
                 }
             }
         }
@@ -108,9 +134,16 @@ fun MentorItem(user: User, onClick: () -> Unit, addToFavorites: () -> Unit) {
 @Composable
 fun DefaultPreview() {
     val user = User(
+        "qwerty123456",
         "https://thispersondoesnotexist.com/image", "John",
         "Doe", "freak", "Senior Dev", "Oracle", true,
-        10.0, arrayOf("JS", "Kotlin"), emptyArray(), arrayOf("BSU"), emptyArray(),
+        10.0, arrayOf("JS", "Kotlin"), emptyArray(), arrayOf("BSU"), emptyArray(), emptyList(),
     )
-    MentorItem(user, onClick = {}, {})
+    setCurrentUser(
+        "qwerty123456",
+        "https://thispersondoesnotexist.com/image", "John",
+        "Doe", "freak", "Senior Dev", "Oracle", true,
+        10.0, arrayOf("JS", "Kotlin"), emptyArray(), arrayOf("BSU"), emptyArray(), emptyList(),
+    )
+    MentorItem(user, onClick = {}, {}, {})
 }
