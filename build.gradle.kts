34603443a04e5e8d4e1ed3da8efe3130a90af329
stasenkots <stasenkots@yandex-team.ru>
2022-06-03 22:57:08 +0300

    refactor root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-       val compose_version = "1.0.0"
 
     repositories {
         google()
@@ -12,12 +11,9 @@ buildscript {
         classpath(BuildPlugins.gradle)
         classpath(BuildPlugins.kotlin)
         classpath(BuildPlugins.hilt)
-
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
     }
 }
 
-tasks.register("clean", Delete::class){
+tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
