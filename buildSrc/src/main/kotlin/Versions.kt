4f0ed214b7adc7f7eb519283026f332d182f8195
stasenkots <stasenkots@yandex-team.ru>
2022-06-03 18:29:31 +0300

    update kotlin and compose versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val gradlePlugin = "7.0.1"
     const val hiltGradlePlugin = "2.38.1"
-    const val kotlin = "1.5.10"
+    const val kotlin = "1.6.10"
     const val parseSdk = "1.25.0"
     const val coroutines = "1.5.1"
     const val navigation = "2.4.0-alpha10"
@@ -9,7 +9,7 @@ object Versions {
     const val hilt = "2.38.1"
     const val lifecycle = "2.3.1"
     const val timber = "5.0.1"
-    const val compose = "1.0.0"
+    const val compose = "1.1.1"
     const val core = "1.6.0"
     const val appCompat = "1.3.1"
     const val activityCompose = "1.3.1"
